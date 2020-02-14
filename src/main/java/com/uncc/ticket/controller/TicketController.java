5c89efc7eda70e1c86e748eb1d2e6d4e1a527cf0
zrypien <32462238+zrypien@users.noreply.github.com>
2020-02-14 13:02:41 -0500

    Second Commit

--- src/main/java/com/uncc/ticket/controller/TicketController.java
+++ src/main/java/com/uncc/ticket/controller/TicketController.java
@@ -52,13 +52,14 @@ public class TicketController {
 
     @RequestMapping(value = "/tickets/edit/{id}", method = RequestMethod.GET)
     public String editTicket(Model model,@PathVariable("id") Long id) {
-        // Code here
-        return "redirect:/"; //Remove this line
+        TicketEntity ticket = ticketService.findById(id);
+        model.addAttribute("ticket",ticket);
+        return "tickets/storeTicket";
     }
 
     @RequestMapping(value = "/tickets/delete/{id}", method = RequestMethod.GET)
     public String deleteTicket(@PathVariable("id") Long id) {
-        // Code here
+           ticketService.deleteById(id);
         return "redirect:/";
     }
 
